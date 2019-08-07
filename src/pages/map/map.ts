51a19832fad9b1579499fb91a65202563b17d7d0
wbd03_01082691@pb.edu.bn <wbd03_01082691@pb.edu.bn>
2019-08-07 11:57:24 +0800

    Usai geolocation: Locate Me & Reset button Usai hotlinee design

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavParams, ViewController } from 'ionic-angular';
+import { NavParams, ViewController, LoadingController, ToastController } from 'ionic-angular';
 import { Location } from '../../models/location.model';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-map',
@@ -10,20 +11,56 @@ export class MapPage {
 
 
 	location: Location = { lat: 4.906883,lng: 114.916486 };
-	marker: Location;
+	marker: Location;  
+  locationIsSet = false;
 
-  constructor(public navParams: NavParams,
+
+  constructor(public navParams: NavParams,        
+          private geoLocation: Geolocation,
+          private loadCtrl: LoadingController,
+          private toast: ToastController,
   			  private viewCtrl: ViewController) {
 
   this.marker = this.location;
   }
 
+  onLocate(){
+    const loading = this.loadCtrl.create({
+      content: 'Getting your location...'
+    });
+    loading.present();
+
+
+    this.geoLocation.getCurrentPosition()
+    .then(
+      location => {
+        loading.dismiss();
+        this.location.lat = location.coords.latitude;
+        this.location.lng = location.coords.longitude;
+        this.marker = new Location(this.location.lat, this.location.lng);
+        this.locationIsSet = true;
+      }
+      )
+      .catch(  // to display errors
+        error => {
+          loading.dismiss();
+          const toast = this.toast.create({
+            message: 'Could not get location, please select it manually!',
+            duration: 2500
+          })
+            toast.present();
+          console.log(error);
+        }
+        );
+      }
+
   onSetMarker(event: any){ 
   	this.marker = new Location(event.coords.lat, event.coords.lng);
+    this.locationIsSet = true;
   }
 
-  onAbort(){
-  	this.viewCtrl.dismiss();
+  onReset(){
+  	this.marker = { lat: 4.906883,lng: 114.916486 };
   }
 
   onConfirm(){
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -10,9 +10,9 @@ import { AlertPage } from '../alert/alert';
 @Component({
   selector: 'page-tabs',
  template: `
-	<ion-tabs selectedIndex="0" class="notetabs">
+	<ion-tabs selectedIndex="0" id="tabs">
 		<ion-tab [root]= "hotlinesPage" tabTitle="Hotlines" tabIcon="md-call"></ion-tab>
-		<ion-tab [root]= "mapPage" tabTitle="Map" tabIcon="md-map"></ion-tab>
+		<ion-tab [root]= "mapPage" tabTitle="Panic" tabIcon="md-map"></ion-tab>
 		<ion-tab [root]= "contactPage" tabTitle="Contact" tabIcon="md-contacts"></ion-tab>	
 		<ion-tab [root]= "alertPage" tabTitle="Alert" tabIcon="md-alert"></ion-tab>			
 	</ion-tabs>
